f0fe4b09deed2040d7734f73a861328a01c4d7ac
akhnasva <akhnaskunnil002@gmail.com>
2022-04-13 16:54:15 +0530

    added

---
+++ projecttodo/todoproject/todoapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from todoapp import views
+
+urlpatterns = [
+    path('',views.add,name="add"),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+]
